eaf4d98918a27a8102cdd070998141371a70cd87
gabx <arnaud.gaboury@gmail.com>
2026-01-19 01:10:18 +0100

    don't use i3 anymore

--- home/config/i3/scripts/load_layout.sh
+++
@@ -1,6 +0,0 @@
-i3-msg "workspace Firefox; append_layout $HOME/.config/i3/layouts/firefox.json"
-i3-msg "workspace Chat; append_layout $HOME/.config/i3/layouts/chat.json"
-sleep 0.5
-(firefox-developer-edition &)
-(slack &)
-
