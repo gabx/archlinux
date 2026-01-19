7d677fa7ab7bc952c7dd6c2c3e2e0aae9cfc2a9b
gabx <arnaud.gaboury@gmail.com>
2026-01-19 01:29:33 +0100

    some changes of my zsh config

--- home/config/zsh/functions/upgrade.r
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env r 
-#
-# a simple example to update packages
-
-repos <- "http://cran.rstudio.com"
-
-lib.loc <- Sys.getenv("R_LIBS_USER")
-
-update.packages(repos=repos, ask=FALSE, lib.loc=lib.loc)
